8997c59edb1405c4e2b50a30f1a9b0bc677acd3e
ZeynepNenseth <70289448+ZeynepNenseth@users.noreply.github.com>
2022-11-15 12:37:22 +0100

    Updated tests

--- src/test/java/org/bank/LoanCalculator/CalculatorTest.java
+++ src/test/java/org/bank/LoanCalculator/CalculatorTest.java
@@ -8,17 +8,25 @@ class CalculatorTest {
 
     @Test
     void hasRight() {
+        int baseAmount4 = 480_000;
+        int baseAmount2 = 240_000;
+        assertTrue(500_000 > baseAmount4);
+        assertTrue(300_000 > baseAmount2);
+        assertFalse(400_000 > baseAmount4);
+        assertFalse(200_000 > baseAmount2);
     }
 
     @Test
     void calculateMaxTotalIncome() {
+        int maxTotalIncome = 120_000 * 6;
+        assertEquals(720_000, maxTotalIncome);
     }
 
     @Test
-    void calculateDailyAllowance() {
-    }
-
-    @Test
-    void getDailyAllowance() {
+    void calculateMonthlyPayback() {
+        int averageIncome = 500_000;
+        int maxTotalIncome = 720_000;
+        int monthlyPayback = 500_000 / 48;
+        assertEquals(monthlyPayback, 10_416);
     }
 }
--- src/test/java/org/bank/LoanCalculator/MainTest.java
+++ src/test/java/org/bank/LoanCalculator/MainTest.java
@@ -6,13 +6,12 @@ class MainTest {
 
     @org.junit.jupiter.api.Test
     void stateIncome() {
-    }
+        int income1 = 200_000;
+        int income2 = 0;
+        int income3 = 800_000;
+        assertEquals(income1, 200_000);
+        assertEquals(income2, 0);
+        assertEquals(income3, 800_000);
 
-    @org.junit.jupiter.api.Test
-    void userInteraction() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void main() {
     }
 }
